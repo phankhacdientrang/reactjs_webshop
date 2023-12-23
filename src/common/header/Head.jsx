7ccbd284daaf5e463a23871c2ae961929317cb82
phankhacdientrang <Trang93190@donga.edu.vn>
2023-12-23 19:54:20 +0700

    up

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> 0343002248</label>
+            <i className='fa fa-envelope'></i>
+            <label> trang93190@donga.edu.vn</label>
+          </div>
+          <div className='right row RText'>
+            <label>Need Help?</label>
+            <span>🏳️‍⚧️</span>
+            <label>EN</label>
+            <span>🏳️‍⚧️</span>
+            <label>VN</label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
